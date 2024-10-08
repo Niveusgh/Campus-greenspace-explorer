d01c1c171dcc5179afd0464fc938c3de45b85104
Thea Francis <mengtinghe@outlook.com>
2024-10-08 00:36:58 -0400

    hardcoded route for favicon path

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from flask import send_from_directory
+import os
 
 app = Flask(__name__)
 
@@ -13,6 +15,11 @@ def map():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static', 'images'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     app.run(debug=True)
